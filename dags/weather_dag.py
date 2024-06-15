3bc4b8392253639aa4484af24e3389cc1b406ea8
pedro.gerolin <pedro.gerolin@pmweb.com.br>
2024-06-15 16:46:57 -0300

    Alteração do end_dt

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -8,17 +8,17 @@ with DAG(
         schedule_interval='@daily' #'0 0 * * 1',  # executar toda segunda feira
 ) as dag:
 
-    city = 'SaoPaulo'
-    key = 'QNSRNJVD9U43K2Y4WXYY8F6LK'
-    start_dt = '{{ ds }}'
-    days = 6
+        city = 'SaoPaulo'
+        key = 'QNSRNJVD9U43K2Y4WXYY8F6LK'
+        start_dt = '{{ ds }}'
+        days = 6
 
-    weather_operator = WeatherOperator(task_id='weather_operator',
-                                       city=city,
-                                       start_dt=start_dt,
-                                       days=days,
-                                       key=key,
-                                       file_path=f'/opt/airflow/files/WeatherAPI/'
-                                                 f'{city}/'
-                                                 f'semana={start_dt}'
-                                                 f'/dados_')
+        weather_operator = WeatherOperator(task_id='weather_operator',
+                                           city=city,
+                                           start_dt=start_dt,
+                                           days=days,
+                                           key=key,
+                                           file_path=f'/opt/airflow/files/WeatherAPI/'
+                                                     f'{city}/'
+                                                     f'semana={start_dt}'
+                                                     f'/dados_')
--- plugins/operators/weather_operator.py
+++ plugins/operators/weather_operator.py
@@ -2,9 +2,10 @@ from datetime import datetime, timedelta
 from pathlib import Path
 from airflow.models import BaseOperator
 from hook.weather_hook import WeatherHook
+from airflow.macros import ds_add
 
 class WeatherOperator(BaseOperator):
-    template_fields = ["start_dt", "end_dt", "file_path"]
+    template_fields = ["start_dt", "file_path"]
 
     def __init__(self, city, start_dt, days, key, file_path, **kwargs):
         super().__init__(**kwargs)
@@ -15,17 +16,14 @@ class WeatherOperator(BaseOperator):
         self.days = days
         self.file_path = file_path
 
-        #self.end_dt = '2024-06-20'
-        self.end_dt = datetime.strptime('2024-06-14', "%Y-%m-%d") + timedelta(days=self.days)
-
     def create_parent_folder(self):
-        print(f"Path usado:{self.file_path}")
         Path(self.file_path).parent.mkdir(parents=True, exist_ok=True)
 
     def execute(self, context):
         self.create_parent_folder()
+        end_dt = ds_add(self.start_dt,self.days)
 
-        weather_data = WeatherHook(self.start_dt, self.end_dt.strftime('%Y-%m-%d'), self.city, self.key)
+        weather_data = WeatherHook(self.start_dt, end_dt, self.city, self.key)
         data = weather_data.run()
         data.to_csv(self.file_path + 'brutos.csv')
         data[['datetime', 'tempmin', 'temp', 'tempmax']].to_csv(self.file_path + 'temperaturas.csv')
