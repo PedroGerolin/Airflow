955c925af83015c5968ebb8df7bea3ed1e7a0e19
pedro.gerolin <pedro.gerolin@pmweb.com.br>
2024-06-17 15:13:29 -0300

    Alteração para usar lista de cidades

--- plugins/operators/weather_operator.py
+++ plugins/operators/weather_operator.py
@@ -5,27 +5,34 @@ from hook.weather_hook import WeatherHook
 from airflow.macros import ds_add
 
 class WeatherOperator(BaseOperator):
-    template_fields = ["start_dt", "file_path"]
+    template_fields = ["start_dt"]
 
-    def __init__(self, city, start_dt, days, key, file_path, **kwargs):
+    def __init__(self, cities, start_dt, days, key, **kwargs):
         super().__init__(**kwargs)
 
-        self.city = city
+        self.cities = cities
         self.key = key
         self.start_dt = start_dt
         self.days = days
-        self.file_path = file_path
+
+    def create_file_path(self, city):
+        self.file_path = f'/opt/airflow/files/WeatherAPI/{city}/semana={self.start_dt}/dados_'
 
     def create_parent_folder(self):
         Path(self.file_path).parent.mkdir(parents=True, exist_ok=True)
 
     def execute(self, context):
-        self.create_parent_folder()
         end_dt = ds_add(self.start_dt,self.days)
+        for city in self.cities:
+            self.create_file_path(city)
+            self.create_parent_folder()
 
-        weather_data = WeatherHook(self.start_dt, end_dt, self.city, self.key)
-        data = weather_data.run()
-        data.to_csv(self.file_path + 'brutos.csv')
-        data[['datetime', 'tempmin', 'temp', 'tempmax']].to_csv(self.file_path + 'temperaturas.csv')
-        data[['datetime', 'description', 'icon']].to_csv(self.file_path + 'condicoes.csv')
+            weather_data = WeatherHook(self.start_dt, end_dt, city, self.key)
+            data = weather_data.run()
+            data.to_csv(self.file_path + 'brutos.csv')
+            data[['datetime', 'tempmin', 'temp', 'tempmax']].to_csv(self.file_path + 'temperaturas.csv')
+            data[['datetime', 'description', 'icon']].to_csv(self.file_path + 'condicoes.csv')
+
+        
+        
 
