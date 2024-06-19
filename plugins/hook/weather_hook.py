8137ef2ce852570bda5208bd71881a2b0bfce21a
pedro.gerolin <pedro.gerolin@pmweb.com.br>
2024-06-19 08:21:17 -0300

    Alterar a conexão para pegar a key

--- plugins/hook/weather_hook.py
+++ plugins/hook/weather_hook.py
@@ -11,24 +11,24 @@ class WeatherHook(HttpHook):
         self.conn_id = conn_id or "weather_api"
         super().__init__(http_conn_id=self.conn_id)
 
-    def create_url(self):
+    def create_url(self, conn):
 
         start_dt = self.start_dt
         end_dt = self.end_dt
         city = self.city
-        key = self.key
+        extra_key = conn.extra_dejson['key']
 
-        URL = (f'{self.base_url}/{city}/{start_dt}/{end_dt}?unitGroup=metric&include=days&key={key}&contentType=csv')
-        print(f"URL será usada:{URL}")
-        return URL
+        url = (f'{conn.host}/{city}/{start_dt}/{end_dt}?unitGroup=metric&include=days&key={extra_key}&contentType=csv')
+        print(f"URL será usada:{url}")
+        return url
 
-    def get_data(self, URL):
-        data = pd.read_csv(URL)
+    def get_data(self, url):
+        data = pd.read_csv(url)
 
         return data
 
     def run(self):
-        session = self.get_conn()
-        URL = self.create_url()
+        conn = self.get_connection(self.conn_id)
+        url = self.create_url(conn)
 
-        return self.get_data(URL)
+        return self.get_data(url)
