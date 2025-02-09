671056554b31642cfde519b3f54ddaa37360bc01
Pedro <pedro_gerolin@yahoo.com.br>
2025-02-09 13:48:37 -0300

    Inclusão dos Dummy Operators

--- dags/WeatherAPI/weather_dag.py
+++ dags/WeatherAPI/weather_dag.py
@@ -1,9 +1,10 @@
 from airflow import DAG
 import pendulum
 from operators.weather_operator import WeatherOperator
+from airflow.operators.dummy_operator import DummyOperator
 
 with DAG(
-        "WeatherAPI",
+        dag_id="WeatherAPI",
         start_date=pendulum.datetime(2025, 2, 1, tz="UTC"),
         schedule_interval='@daily' #'0 0 * * 1',  # executar toda segunda feira
 ) as dag:
@@ -16,3 +17,15 @@ with DAG(
                                         cities=cities,
                                         start_dt=start_dt,
                                         days=days)
+
+start_task = DummyOperator(
+    task_id = 'start_task',
+    dag = dag
+)    
+
+end_task = DummyOperator(
+    task_id = 'end_task',
+    dag = dag
+)    
+
+start_task >> weather_operator >> end_task
